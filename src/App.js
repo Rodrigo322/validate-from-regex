73f4dbda00fbb519c26c64bf6c7e4b240d34ba1d
Rodrigo322 <rodrigollrdglucas@gmail.com>
2021-11-08 15:12:43 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+
+import { validEmail, validPassword } from "./utils/regex";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [inputEmailErr, setInputEmailErr] = useState(false);
+  const [inputPassordErr, setInputPasswordErr] = useState(false);
+
+  const validate = () => {
+    if (!validEmail.test(email)) {
+      setInputEmailErr(true);
+    } else {
+      setInputEmailErr(false);
+    }
+    if (!validPassword.test(password)) {
+      setInputPasswordErr(true);
+    } else {
+      setInputPasswordErr(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Validando form com RegEx</h1>
+      
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+      />
+      {inputEmailErr && <p>Por favor digete um email valido!</p>}
+
+      <input
+        type="text"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      {inputPassordErr && <p>Por favor digite uma senha mais segura</p>}
+
+      <button onClick={validate}>Validar</button>
+
     </div>
   );
 }
